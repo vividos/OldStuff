c7b4ac0e9d4975c5f288acfe4043c267cdce0c58
Michael Fink <michael.fink@asamnet.de>
2016-10-09 23:17:10 +0200

    added Android projects for Intro

---
+++ C64/src/Intro/Android/src/de/vividos/c64/intro/MainActivity.java
@@ -0,0 +1,16 @@
+
+package de.vividos.c64.intro;
+
+import android.app.Activity;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class MainActivity extends org.libsdl.app.SDLActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
