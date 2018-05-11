12ada75668f1a22a233fc1af6749419d70a34e24
Michael Fink <michael.fink@asamnet.de>
2018-05-11 22:44:25 +0200

    added call to pucrunch.exe in order to get smaller .prg's

--- C64/TheLastIntro/Standalone/Build.cmd
+++ C64/TheLastIntro/Standalone/Build.cmd
@@ -5,11 +5,12 @@ REM (c) 2018 vividos
 REM
 REM -------------------------------------------------------------	
 
-set PATH=%PATH%;%CD%\..\tools\cc65\bin;%CD%\..\tools\winVICE
+set PATH=%PATH%;%CD%\..\tools\cc65\bin;%CD%\..\tools\winVICE;%CD%\..\tools\pucrunch\
 
 ca65 ..\shared\intro.s -o intro-standalone.o -D EXIT_ACTION_SOFT_RESET -t c64
 ca65 scrolltext.s -t c64
 ld65 -C intro.cfg intro-standalone.o scrolltext.o -o intro.prg
+pucrunch.exe -f -c64 -i1 -g0x37 intro.prg intro-pucrunch.prg
 c1541 < c1541script.txt
 
 pause
