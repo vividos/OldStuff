6f08390d90bc1a0eee00c5867cad236ee51d2dac
Michael Fink <michael.fink@asamnet.de>
2018-07-05 20:43:50 +0200

    improved building standalone intro and added clean cmd script

--- C64/TheLastIntro/Standalone/Build.cmd
+++ C64/TheLastIntro/Standalone/Build.cmd
@@ -5,12 +5,14 @@ REM (c) 2018 vividos
 REM
 REM -------------------------------------------------------------	
 
+REM Build.cmd - Build script for the standalone intro
+
 set PATH=%PATH%;%CD%\..\tools\cc65\bin;%CD%\..\tools\winVICE;%CD%\..\tools\pucrunch\
 
 ca65 ..\shared\intro.s -o intro-standalone.o -D EXIT_ACTION_SOFT_RESET -t c64
 ca65 scrolltext.s -t c64
-ld65 -C intro.cfg intro-standalone.o scrolltext.o -o intro.prg
-pucrunch.exe -f -c64 -i1 -g0x37 intro.prg intro-pucrunch.prg
+ld65 -C intro.cfg intro-standalone.o scrolltext.o -o intro-standalone.prg
+pucrunch.exe -f -c64 -i1 -g0x37 intro-standalone.prg intro-standalone-pucrunch.prg
 c1541 < c1541script.txt
 
 pause
---
+++ C64/TheLastIntro/Standalone/Clean.cmd
@@ -0,0 +1,14 @@
+REM -------------------------------------------------------------	
+REM
+REM vividos' last intro
+REM (c) 2018 vividos
+REM
+REM -------------------------------------------------------------	
+
+REM Clean.cmd - Clean script for the standalone intro
+
+del intro-standalone.o
+del scrolltext.o
+del intro-standalone.prg
+del intro-standalone-pucrunch.prg
+del intro-vividos.d64
