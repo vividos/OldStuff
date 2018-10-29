ca0849b5b1b02928c930287ed623ba66193d4980
Michael Fink <michael.fink@asamnet.de>
2018-10-29 19:28:15 +0100

    added clean script for CommonDisk and ignored output

---
+++ C64/TheLastIntro/CommonDisk/Clean.cmd
@@ -0,0 +1,10 @@
+REM -------------------------------------------------------------	
+REM
+REM vividos' last intro
+REM (c) 2018 vividos
+REM
+REM -------------------------------------------------------------	
+
+REM Clean.cmd - Clean script for the CommonDisk project
+
+del commondisk-vividos.d64
